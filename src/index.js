98a19e8734c60b0adc864f359b7b4be94f9f4ddc
qzheng <sdddev@master-dynamic.com>
2019-10-03 11:07:24 +0800

    iPhone X screen height hot fix

--- src/index.js
+++ src/index.js
@@ -263,7 +263,24 @@ export default class extends Component {
     }
 
     // Default: horizontal
-    const { width, height } = Dimensions.get('window')
+    // const { width, height } = Dimensions.get('window')
+    // now need to handle iPhone get window height not correct issue.
+    // ------------------------------------------------------------------
+    const X_WIDTH = 375;
+    const X_HEIGHT = 812;
+
+    const XSMAX_WIDTH = 414;
+    const XSMAX_HEIGHT = 896;
+    const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get('screen');
+    let isIPhoneX = false;
+    if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
+      isIPhoneX = W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT || W_WIDTH === XSMAX_WIDTH && W_HEIGHT === XSMAX_HEIGHT;
+    }
+
+    const width  =  Dimensions.get('window').width;
+    const height =  Dimensions.get('screen').height - (isIPhoneX?78:0);
+
+    // ------------------------------------------------------------------
 
     initState.dir = props.horizontal === false ? 'y' : 'x'
 
